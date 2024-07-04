0c3a7f65ed1c01883f1086640094ba1297005a33
JesusMartinezClavel <jmcvalles@gmail.com>
2024-07-04 17:31:54 +0200

    FEAT -> Updated form component to input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // REACT
-import { useState } from "react";
+import React, { useState } from "react";
 import { X } from "lucide-react";
 
 // STYLES
@@ -11,7 +11,7 @@ import { CButton } from "./components/cButton/cButton";
 import { CCard } from "./components/cCard/cCard";
 import { CInput } from "./components/cForm/cInput";
 
-const App = () => {
+const App: React.FC = () => {
   const [show, setShow] = useState({
     sendEmail: false,
   });
@@ -27,14 +27,30 @@ const App = () => {
       : setShow({ sendEmail: true });
     console.log(show);
     console.log(personalInfo);
-    
   };
 
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInput = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     setPersonalInfo((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
+  };
+
+  // const clearInput = (
+  //   e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  // ) => {
+  //   setPersonalInfo((prevState) => ({
+  //     ...prevState,
+  //     [e.target.name]: "",
+  //   }));
+  // };
+
+  console.log(personalInfo);
+
+  const ok = () => {
+    console.log("no");
   };
 
   return (
@@ -48,7 +64,34 @@ const App = () => {
           onClick={showSendEmail}
         />
         <CCard className={show.sendEmail ? "card-sendEmail" : "hidden"}>
-          <CInput className="" type='email' name='name' value='value'/>
+          <CInput
+            type="text"
+            name="name"
+            placeholder="input name"
+            className=""
+            value={personalInfo.name || ""}
+            onChange={handleInput}
+            onBlur={ok}
+          />
+          <CInput
+            type="email"
+            name="email"
+            placeholder="input email"
+            className=""
+            value={personalInfo.email || ""}
+            onChange={handleInput}
+            onBlur={ok}
+          />
+          <CInput
+            type="textarea"
+            name="message"
+            placeholder="input message"
+            className=""
+            value={personalInfo.message || ""}
+            onChange={handleInput}
+            onBlur={ok}
+          />
+          <CButton title="concertar cita" className="" onClick={ok} disabled/>
         </CCard>
         <p>**texto legal**</p>
       </CCard>
--- src/components/cForm/cInput.tsx
+++ src/components/cForm/cInput.tsx
@@ -2,53 +2,49 @@ import "./cInput.css";
 
 interface CFormProps {
   className: string;
-  inputs: {
-    type: string;
-    name: string;
-    value: string;
-    placeholder: string;
-    onChange: (
-      e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-    ) => void;
-    onBlur: (
-      e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
-    ) => void;
-  }[];
+  type: string;
+  name: string;
+  value: string;
+  placeholder: string;
+  onChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  onBlur: (e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
 }
 
-export const CInput: React.FC<CFormProps> = ({ className, inputs }) => {
+export const CInput: React.FC<CFormProps> = ({
+  className,
+  type,
+  name,
+  placeholder,
+  value,
+  onChange,
+  onBlur,
+}) => {
   const combinedClasses = `input-design ${className || ""}`;
 
   return (
-    <div className={combinedClasses}>
-      {inputs.map((input) => {
-        if (input.type === "textarea") {
-          return (
-            <textarea
-              className={className}
-              name={input.name}
-              value={input.value}
-              placeholder={input.placeholder}
-              onChange={input.onChange}
-              onBlur={input.onBlur}
-              rows={4}
-              required
-            />
-          );
-        }
-        return (
-          <input
-            className={className}
-            type={input.type}
-            name={input.name}
-            value={input.value}
-            placeholder={input.placeholder}
-            onChange={input.onChange}
-            onBlur={input.onBlur}
-            required
-          />
-        );
-      })}
-    </div>
+    <>
+      {type === "textarea" ? (
+        <textarea
+          name={name}
+          placeholder={placeholder}
+          className={combinedClasses}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+      ) : (
+        <input
+          type={type}
+          name={name}
+          placeholder={placeholder}
+          className={combinedClasses}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+      )}
+    </>
   );
 };
